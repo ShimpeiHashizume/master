518e52a213dee889c4a8d7ec2a2843de84bc1ae9
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-01-25 22:53:41 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // li生成
+  const li = document.createElement("li");
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // liタグの子要素に各要素を設定
+  li.appendChild(div);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(p);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
