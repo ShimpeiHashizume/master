901cd2899574dc7db0cb54f0fadba3be25dc7669
ShimpeiHashizume <69233878+ShimpeiHashizume@users.noreply.github.com>
2022-01-27 23:11:28 +0900

    戻すボタン実装

--- src/index.js
+++ src/index.js
@@ -5,6 +5,16 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // li生成
   const li = document.createElement("li");
 
@@ -14,7 +24,7 @@ const onClickAdd = () => {
 
   // pタグ生成
   const p = document.createElement("p");
-  p.innerText = inputText;
+  p.innerText = text;
 
   // button(完了)タグ生成
   const completeButton = document.createElement("button");
@@ -39,6 +49,15 @@ const onClickAdd = () => {
     // buttonタグ生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ(li)を完了リストから削除
+      const deleteTarget = backButton.closest("li");
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
 
     // divの子要素に各要素を設定
     addTarget.firstElementChild.appendChild(p);
@@ -67,11 +86,6 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
